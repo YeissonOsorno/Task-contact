e28707fdaaec64d704b1c430463ff7c8e20c01db
Yeisson Osorno <yeisson403@gmail.com>
2020-03-13 00:22:45 -0500

    App finished

---
+++ README.md
@@ -0,0 +1,5 @@
+Resources
+https://firebase.google.com/docs/admin/setup
+https://firebase.google.com/docs/firestore/manage-data/delete-data
+https://freefrontend.com/css-background-patterns/
+https://www.bootstrapcdn.com/bootswatch/
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,12 +1,39 @@
 const {Router} = require('express');
 const router =  Router();
+const admin = require('firebase-admin');
+
+//Credendciales para conectar e inicializar mi app con firebase
+admin.initializeApp({
+   credential: admin.credential.applicationDefault(),
+   databaseURL : 'https://fir-with-nodejs.firebaseio.com/'   
+});
+
+// Conectar con mi database
+const db = admin.database();
 
 router.get('/',(req,res)=>{
-   res.render('index')
+   db.ref('contacts').once('value',(snapshot) =>{
+      const data = snapshot.val();
+      res.render('index',{contacts : data})
+   })
 });
 
 router.post('/new-contact',(req,res)=>{
    console.log(req.body);
-   res.send('Received');
+   const newContact = {
+      firstname : req.body.firstname,
+      lastname : req.body.lastname,
+      email :req.body.email,
+      phone : req.body.phone      
+   }
+   //Nombre de la colección
+   db.ref('contacts').push(newContact)
+   
+   res.redirect('/');
+})
+
+router.get('/delete-contact/:id',(req,res)=>{
+   db.ref('contacts/' + req.params.id).remove();
+   res.redirect('/');
 })
 module.exports = router;
